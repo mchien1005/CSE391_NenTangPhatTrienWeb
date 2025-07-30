6093aab1e144f2bcd8e2723bda27ad930786ad24
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-07-30 23:07:48 +0700

    đề 2

---
+++ 2/data.js
@@ -0,0 +1,37 @@
+const giaoDichData = [
+    {
+        id: 1101,
+        khachHang: "Nguyễn Văn A",
+        nhanVien: "Lê Thị B",
+        soTien: 500000,
+        ngayMua: "06 Tháng 5 2024 08:00"
+    },
+    {
+        id: 1102,
+        khachHang: "Trần Minh C",
+        nhanVien: "Phạm Duy D",
+        soTien: 250000,
+        ngayMua: "06 Tháng 5 2024 09:00"
+    },
+    {
+        id: 1103,
+        khachHang: "Phạm Ngọc E",
+        nhanVien: "Vũ Hải F",
+        soTien: 650000,
+        ngayMua: "06 Tháng 5 2024 10:15"
+    },
+    {
+        id: 1104,
+        khachHang: "Lê Thị G",
+        nhanVien: "Nguyễn Văn H",
+        soTien: 300000,
+        ngayMua: "06 Tháng 5 2024 11:30"
+    },
+    {
+        id: 1105,
+        khachHang: "Hoàng Gia I",
+        nhanVien: "Trần Thị J",
+        soTien: 750000,
+        ngayMua: "06 Tháng 5 2024 12:00"
+    }
+];
